2feca6b0d03e63e214a5c0145a6a77dc5857c3b2
Mel Rabineau <melanie.rabineau1@gmail.com>
2016-10-10 12:29:53 -0700

    slow progress

--- js/style.js
+++ js/style.js
@@ -3,23 +3,40 @@ console.log("game!")
 
 var answer = ["S", "H", "I", "P"]
 var answer2 = ["P", "I", "R", "A", "T", "E"]
-var guess = ["R"]
+// var guess = document.querySelector('#screen').textContent;
+var guess = ""
+var win = false
+var lose
+
+$("#screen").on('keydown', function (e) {
+  // console.log(e.keyCode)
+  guess = String.fromCharCode(e.keyCode)
+
+})
+
 
 function theAnswer() {
   for (var i = 0; i < answer.length; i ++) {
-    if (guess[0] !== answer[0])
-    return false
+  if (guess === answer[0])
+    return true
+
+
+  document.querySelector("#screen").textContent = guess
+
   }
 
-  if (guess[0] === answer[0])
-    return
+    if (guess !== answer[0])
+    // this.remove(guess"0")
 
 
 }
 
-theAnswer();
 
-function theAnswer2 () {
+
+theAnswer();
+// guess[0].onkyepress=function()
+
+function theAnswer2() {
   for (var i = 0; i < answer.length; i ++) {
     if (guess[0] !== answer2[0])
       return false
